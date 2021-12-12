fe4945ac15abd5c2303a04b0fedbd4ba6aba27c0
Rostyslaw Kostyk <rkostyk64@gmail.com>
2021-12-12 23:42:02 +0200

    Секция, фидбэк

---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,14 @@
+import styles from "./Statistics.module.css";
+import PropTypes from "prop-types";
+
+const Statistics = () => {
+    return (
+
+    )
+}
+
+Statistics.prototype = {
+    
+}
+
+export default Statistics;
